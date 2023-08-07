ff49e86a9ef3342dadb265c5384283269b5ee67e
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-07 22:53:15 +0300

    Configure docker-based postgresql
    * Add Dockerfile
    * Add docker-compose.yml
    * Add ddl.sql and dml.sql
    * Add init.sql

---
+++ db/dml.sql
@@ -0,0 +1,85 @@
+INSERT INTO
+    Person(name, yearofbirth)
+VALUES ('Петров Петр Петрович', 1987);
+
+INSERT INTO
+    Person(name, yearofbirth)
+VALUES ('Степашко Артемий Евгеньевич', 1955);
+INSERT INTO
+    Person(name, yearofbirth)
+VALUES ('Коловрат Евпатий Адрианович', 2001);
+INSERT INTO
+    Person(name, yearofbirth)
+VALUES ('Джон Джонс Младший', 1977);
+
+
+
+
+INSERT INTO
+    Book
+    (name,
+     authorname,
+     yearofpublishing)
+VALUES ('Превращение',
+        'Франц Кафка',
+        1915);
+
+INSERT INTO
+    Book
+(name,
+ authorname,
+ yearofpublishing)
+VALUES ('Дон Кихот',
+        'Мигель де Сервантес',
+        1615);
+INSERT INTO
+    Book
+(name,
+ authorname,
+ yearofpublishing)
+VALUES ('Преступление и наказание',
+        'Федор Михайлович Достоевский',
+        1866);
+INSERT INTO
+    Book
+(name,
+ authorname,
+ yearofpublishing)
+VALUES ('Процесс',
+        'Франц Кафка',
+        1925);
+
+INSERT INTO
+    Book
+    (name,
+     authorname,
+     yearofpublishing,
+     personid)
+VALUES ('Америка',
+        'Франц Кафка',
+        1927,
+        1);
+
+
+INSERT INTO
+    Book
+(name,
+ authorname,
+ yearofpublishing,
+ personid)
+VALUES ('Generation "P"',
+        'Виктор Олегович Пелевин',
+        1999,
+        2);
+
+
+INSERT INTO
+    Book
+(name,
+ authorname,
+ yearofpublishing,
+ personid)
+VALUES ('Чапаев и пустота',
+        'Виктор Олегович Пелевин',
+        1927,
+        2);
