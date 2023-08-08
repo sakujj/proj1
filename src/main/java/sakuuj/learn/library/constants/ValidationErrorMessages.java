a4262bed8ba98d3a0eb280dc785746461b4ab09b
Pavel Rysnik <pavelrysnik@gmail.com>
2023-08-08 13:48:23 +0300

    Add ValidationErrorMessages in constants

---
+++ src/main/java/sakuuj/learn/library/constants/ValidationErrorMessages.java
@@ -0,0 +1,15 @@
+package sakuuj.learn.library.constants;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ValidationErrorMessages {
+    public static final String ON_EMPTY = "Empty value is not allowed";
+    public static final String ON_BLANK = "Blank value is not allowed";
+    public static final String ON_SMALL_LENGTH = "Entered value's length is too small";
+    public static final String ON_LARGE_LENGTH = "Entered value's length is too large";
+
+    public static final String ON_SMALL_VALUE = "Entered value is too small";
+
+    public static final String ON_PERSON_NAME_ALREADY_TAKEN = "Entered name is already taken";
+}
